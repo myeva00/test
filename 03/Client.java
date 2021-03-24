ba91244bd283e7d3c52082bdb8b13baf597c9eb6
myeva00 <1138252245@qq.com>
2021-03-24 23:45:49 +0800

    第三次作业

---
+++ 03/Client.java
@@ -0,0 +1,10 @@
+public class Client {
+    public static void main(String args[]) {
+        Image image;
+        ImageImp imp;
+        image = (Image)XMLUtil.getBean("image");
+        imp = (ImageImp)XMLUtil.getBean("os");
+        image.setImageImp(imp);
+        image.parseFile("boys will be boys");
+    }
+} 
